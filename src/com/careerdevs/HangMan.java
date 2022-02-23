6510f55672fef6851a1509e06a5d43c39b4c6e3f
Keith Benjamin <kjb528i@yahoo.com>
2022-02-22 19:01:03 -0500

    added hangman class

--- src/com/careerdevs/HangMan.java
+++ src/com/careerdevs/HangMan.java
@@ -1,9 +1,11 @@
 package com.careerdevs;
 
 
+import jdk.swing.interop.SwingInterOpUtils;
+
 public class HangMan {
 
-    String[] playableWords = new String[] {
+    public static String[] playableWords = new String[]{
             "guessing", "these", "words",
             "is", "very", "easy"
     };
@@ -23,16 +25,25 @@ public class HangMan {
         }
     }
 
-    public static void startGame(){
-
+    public static void startGame() {
+        promptUser();
     }
 
-    public static void promptUser(){
+    public static void promptUser() {
+        int num = (int) (Math.random() * (7 - 1) + 1);
 
-
-
+        // System.out.println(playableWords[num -1 ]);
+        String word = playableWords[num - 1];
+        char[] letters = word.toCharArray();
+        //System.out.println(word);
+        String usersGuess = UI.readString("Please enter a letter");
+        char guessedLetter = usersGuess.charAt(0);
+        // System.out.println(guessedLetter);
+        System.out.println(letters.length);
+        for (int i = 0; 0 < letters.length; i++) {
+            System.out.print(letters[i] + " ");
         }
+
     }
-
-
 }
+
